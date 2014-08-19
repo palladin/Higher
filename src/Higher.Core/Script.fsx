d9b1113bc79a6d337b51e1da7a1cf3fd5a67a7ed
npal <nick.palladinos@gmail.com>
2014-08-19 18:39:44 +0300

    Experimenting with Monads

--- src/Higher.Core/Script.fsx
+++ src/Higher.Core/Script.fsx
@@ -1,10 +1,11 @@
 ﻿
-
+// Represents type application
 type App<'F, 'T> = App of 'F
 type App2<'F, 'T1, 'T2> = App<App<'F, 'T1>, 'T2>
 type App3<'F, 'T1, 'T2, 'T3> = App<App2<'F, 'T1, 'T2>, 'T3>
 type App4<'F, 'T1, 'T2, 'T3, 'T4> = App<App3<'F, 'T1, 'T2, 'T3>, 'T4>
 
+// Basic Types App Inj/Prj
 type List = private F of obj with
     static member Inj (value : 'T list) : App<List, 'T> = App (F value)
     static member Prj (app : App<List, 'T>) : 'T list = 
@@ -18,10 +19,24 @@ type Option = private F of obj with
         value :?> _
 
 
-let foo = List.Inj [1..100]
-List.Prj foo
-
-let foo = Option.Inj <| Some 1
-Option.Prj foo
 
 
+[<AbstractClass>]
+type Monad<'M>() = 
+    abstract Return<'T> : 'T -> App<'M, 'T>
+    abstract Bind<'T, 'R> : App<'M, 'T> -> ('T -> App<'M, 'R>) -> App<'M, 'R>
+
+
+type OptionMonad() = 
+    inherit Monad<Option>() with
+        override self.Return x = Option.Inj <| Some x
+        override self.Bind m f = 
+            match Option.Prj m with
+            | Some v -> f v
+            | None -> Option.Inj None
+
+
+module Monad = 
+    
+    let sequence (d : Monad<'M>) (ms :  App<'M, 'T> list) : App<'M, 'T list> = 
+        raise <| new System.NotImplementedException()
