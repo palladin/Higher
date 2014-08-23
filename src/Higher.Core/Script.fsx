062e821bd543441ec2224ae6e1d7bbd257302f2a
palladin <nick.palladinos@gmail.com>
2014-08-23 12:59:29 +0300

    Inj/Prf token parity

--- src/Higher.Core/Script.fsx
+++ src/Higher.Core/Script.fsx
@@ -1,8 +1,11 @@
 ﻿
 // Represents type application
-type App<'F, 'T> (value : obj) =
+type App<'F, 'T> (token : 'F, value : obj) =
     // Apply the secret token to have access the encapsulated value
-    member self.Apply(token : 'F) = value 
+    member self.Apply(token' : 'F) =
+        if System.Object.ReferenceEquals(token, token') then  
+            value 
+        else raise <| new System.InvalidOperationException("Invalid token")
 type App2<'F, 'T1, 'T2> = App<App<'F, 'T1>, 'T2>
 type App3<'F, 'T1, 'T2, 'T3> = App<App2<'F, 'T1, 'T2>, 'T3>
 type App4<'F, 'T1, 'T2, 'T3, 'T4> = App<App3<'F, 'T1, 'T2, 'T3>, 'T4>
@@ -12,14 +15,14 @@ type App4<'F, 'T1, 'T2, 'T3, 'T4> = App<App3<'F, 'T1, 'T2, 'T3>, 'T4>
 type List private () =
     static member private token = new List()  
     static member Inj (value : 'T list) : App<List, 'T> = 
-        new App<_, _>(value)
+        new App<_, _>(List.token, value)
     static member Prj (app : App<List, 'T>) : 'T list = 
         app.Apply(List.token) :?> _
 
 type Option private () =
     static member private token = new Option()
     static member Inj (value : 'T option) : App<Option, 'T> = 
-        new App<_, _>(value)
+        new App<_, _>(Option.token, value)
     static member Prj (app : App<Option, 'T>) : 'T option = 
         app.Apply(Option.token) :?> _
 
