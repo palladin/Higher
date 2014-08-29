a6a1700aa92170ff61174be563166415193444b4
palladin <nick.palladinos@gmail.com>
2014-08-29 18:35:50 +0300

    Monad functions

---
+++ src/Higher.Core/Monad.fs
@@ -0,0 +1,61 @@
+﻿namespace Higher.Core
+
+// Monad Class and helper functions
+[<AbstractClass>]
+type Monad<'M>() = 
+    abstract Return<'T> : 'T -> App<'M, 'T>
+    abstract Bind<'T, 'R> : App<'M, 'T> *  ('T -> App<'M, 'R>) -> App<'M, 'R>
+
+// Generic Monad functions 
+module Monad = 
+
+    let rec sequence (monad : Monad<'M>) (ms :  App<'M, 'T> list) : App<'M, 'T list> =
+        match ms with
+        | m :: ms -> 
+            monad {
+                let! x = m
+                let! xs = sequence monad ms
+                return x :: xs
+            }
+        | [] -> monad { return [] }
+
+    let mapM (monad : Monad<'M>) (f : 'T -> App<'M, 'R>) (xs : 'T list) : App<'M, 'R list> =
+        xs |> List.map f |> sequence monad
+
+    let rec filterM (monad : Monad<'M>) (f : 'T -> App<'M, bool>) (xs : 'T list) : App<'M, 'T list> =
+        match xs with
+        | x :: xs -> 
+            monad {
+                let! flag = f x
+                let! xs' = filterM monad f xs
+                return if flag then x :: xs' else xs'
+            }
+        | [] -> monad { return [] }
+
+// Basic Monad instances
+type ListMonad() = 
+    inherit Monad<List>() with
+        override self.Return x = List.Inj [x]
+        override self.Bind (m, f) =
+            m
+            |> List.Prj 
+            |> List.collect (fun v -> List.Prj (f v))  
+            |> List.Inj 
+
+type SeqMonad() = 
+    inherit Monad<Seq>() with
+        override self.Return x = Seq.Inj [x]
+        override self.Bind (m, f) =
+            m
+            |> Seq.Prj 
+            |> Seq.collect (fun v -> Seq.Prj (f v))  
+            |> Seq.Inj 
+            
+type OptionMonad() = 
+    inherit Monad<Option>() with
+        override self.Return x = Option.Inj <| Some x
+        override self.Bind (m, f) = 
+            match Option.Prj m with
+            | Some v -> f v
+            | None -> Option.Inj None
+
--- src/Higher.Core/Script.fsx
+++ src/Higher.Core/Script.fsx
@@ -1,81 +1,11 @@
 ﻿
-open System
-
-// Represents type application
-type App<'F, 'T> (token : 'F, value : obj) =
-    do 
-        if Object.ReferenceEquals(token,  Unchecked.defaultof<'F>) then
-            raise <| new System.InvalidOperationException("Invalid token")
-    // Apply the secret token to have access to the encapsulated value
-    member self.Apply(token' : 'F) =
-        if Object.ReferenceEquals(token, token') then  
-            value 
-        else raise <| new InvalidOperationException("Invalid token")
-type App2<'F, 'T1, 'T2> = App<App<'F, 'T1>, 'T2>
-type App3<'F, 'T1, 'T2, 'T3> = App<App2<'F, 'T1, 'T2>, 'T3>
-type App4<'F, 'T1, 'T2, 'T3, 'T4> = App<App3<'F, 'T1, 'T2, 'T3>, 'T4>
-
-// Basic Type Constructors with Inj/Prj operations
-// To ensure type-safety we use a secret token based control access policy. 
-type List private () =
-    static let token = new List()  
-    static member Inj (value : 'T list) : App<List, 'T> = 
-        new App<_, _>(token, value)
-    static member Prj (app : App<List, 'T>) : 'T list = 
-        app.Apply(token) :?> _
-
-type Seq private () =
-    static let token = new Seq()  
-    static member Inj (value : seq<'T>) : App<Seq, 'T> = 
-        new App<_, _>(token, value)
-    static member Prj (app : App<Seq, 'T>) : seq<'T> = 
-        app.Apply(token) :?> _
-
-type Option private () =
-    static let token = new Option()
-    static member Inj (value : 'T option) : App<Option, 'T> = 
-        new App<_, _>(token, value)
-    static member Prj (app : App<Option, 'T>) : 'T option = 
-        app.Apply(token) :?> _
+#load "CoreTypes.fs"
+open Higher.Core
+#load "Monad.fs"
+open Higher.Core
 
 
-// Monad Class and helper functions
-[<AbstractClass>]
-type Monad<'M>() = 
-    abstract Return<'T> : 'T -> App<'M, 'T>
-    abstract Bind<'T, 'R> : App<'M, 'T> -> ('T -> App<'M, 'R>) -> App<'M, 'R>
 
-module Monad = 
-
-    let sequence (d : Monad<'M>) (ms :  App<'M, 'T> list) : App<'M, 'T list> = 
-        raise <| new System.NotImplementedException()
-
-// Basic Monad instances
-type ListMonad() = 
-    inherit Monad<List>() with
-        override self.Return x = List.Inj [x]
-        override self.Bind m f =
-            m
-            |> List.Prj 
-            |> List.collect (fun v -> List.Prj (f v))  
-            |> List.Inj 
-
-type SeqMonad() = 
-    inherit Monad<Seq>() with
-        override self.Return x = Seq.Inj [x]
-        override self.Bind m f =
-            m
-            |> Seq.Prj 
-            |> Seq.collect (fun v -> Seq.Prj (f v))  
-            |> Seq.Inj 
-            
-type OptionMonad() = 
-    inherit Monad<Option>() with
-        override self.Return x = Option.Inj <| Some x
-        override self.Bind m f = 
-            match Option.Prj m with
-            | Some v -> f v
-            | None -> Option.Inj None
 
 
 
