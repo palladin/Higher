09bccab5a0bcfcf0614f322575f53c110d20eb0a
palladin <nick.palladinos@gmail.com>
2014-09-06 17:51:26 +0300

    New Traversable Type Class

--- src/Higher.Core/List.fs
+++ src/Higher.Core/List.fs
@@ -26,3 +26,14 @@ type ListApplicative() =
         let fs, xs = List.Prj appF, List.Prj app
         xs |> List.zip fs |> List.map (fun (f, x) -> f x) |> List.Inj
 
+
+
+// List Traversable instance
+type ListTraversable() = 
+     inherit Traversable<List>() with
+     override self.Map f func = 
+        func |> List.Prj |> List.map f |> List.Inj
+     override self.Traverse<'F, 'T, 'R> (app : Applicative<'F>) (f : 'T -> App<'F, 'R>) (trav : App<List, 'T>) = 
+        let xs = trav |> List.Prj |> List.map f
+        let appCons = app.Pure (fun (x : 'R) xs -> List.Inj (x :: (List.Prj xs)))
+        List.foldBack (fun appX appXs -> app.Apply (app.Apply appCons appX) appXs) xs (app.Pure (List.Inj []))
---
+++ src/Higher.Core/Traversable.fs
@@ -0,0 +1,9 @@
+﻿namespace Higher.Core
+
+// Traversable Class
+[<AbstractClass>]
+type Traversable<'TR>() = 
+    inherit Functor<'TR>() 
+    abstract Traverse<'F, 'T, 'R> : Applicative<'F> -> ('T -> App<'F, 'R>) -> App<'TR, 'T> -> App<'F, App<'TR, 'R>>
+    member self.SequenceA applicative trav =
+        self.Traverse applicative id trav 
