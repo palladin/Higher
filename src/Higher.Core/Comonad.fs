64f781882fee764a5f837fd72ca131704efb2bb1
leo <eulerfx@gmail.com>
2015-04-25 16:30:57 -0400

    comonad

---
+++ src/Higher.Core/Comonad.fs
@@ -0,0 +1,17 @@
+﻿namespace Higher.Core
+
+// Comonad Class 
+[<AbstractClass>]
+type Comonad<'W>() = 
+    inherit Functor<'W>()
+    override self.Map (f : 'A -> 'B) (func : App<'W, 'A>) =
+      self.Extend (self.Extract >> f) func
+    abstract Extract<'A> : App<'W, 'A> -> 'A
+    abstract Extend<'A, 'B> : (App<'W, 'A> -> 'B) -> App<'W, 'A> -> App<'W, 'B>
+
+
+// Generic comonad functions.
+module Comonad =
+  
+    let duplicate (comonad : Comonad<'W>) (w : App<'W, 'A>) : App<'W, App<'W, 'A>> =
+        comonad.Extend id w
