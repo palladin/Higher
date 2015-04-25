6bfd0e6b4863d0a6cc07e9d31e397ce93ca55ab1
leo <eulerfx@gmail.com>
2015-04-25 17:37:53 -0400

    algebra, fix point

---
+++ src/Higher.Core/Fix.fs
@@ -0,0 +1,27 @@
+﻿namespace Higher.Core
+
+/// Fix point.
+type Fix<'F> = Fix of App<'F, Fix<'F>>
+
+type Fix private () =
+  static let token = new Fix()    
+  static member Inj (value : Fix<'F>) : App<Fix, 'F> =
+    new App<Fix, 'F>(token, value)  
+  static member Prj (app2 : App<Fix, 'F>) : Fix<'F> =
+    app2.Apply(token) :?> _
+
+type Fix with
+  
+  static member un (Fix(f)) = f
+
+  static member cata (func : Functor<'F> ) (alg : Algebra<'F, 'A>) (fix : Fix<'F>) : 'A =
+    fix
+    |> Fix.un
+    |> func.Map (Fix.cata func alg)
+    |> alg
+
+  static member ana (func : Functor<'F>) (coalg : CoAlgebra<'F, 'A>) (a : 'A) : Fix<'F> =
+    a
+    |> coalg
+    |> func.Map (Fix.ana func coalg)
+    |> Fix.Fix
