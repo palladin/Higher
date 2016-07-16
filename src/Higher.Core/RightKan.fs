6a3fe71e98d035e9767d8882dc3fabd029b49370
Nick Palladinos <nick.palladinos@gmail.com>
2016-07-16 16:28:18 +0300

    Add Right Kan type and functor instance

---
+++ src/Higher.Core/RightKan.fs
@@ -0,0 +1,32 @@
+﻿namespace Higher.Core
+
+// type Ran g h α = R (∀β. (α → g β) → h β)
+type Ran<'G, 'H, 'A> =
+    abstract Invoke<'B> : ('A -> App<'G, 'B>) -> App<'H, 'B>
+
+
+type Ran private () =    
+    static let token = new Ran()
+    static member Inj (value : Ran<'G, 'H, 'A>) : App3<Ran, 'G, 'H, 'A> = 
+        let app = new App<Ran, 'G>(token, value)
+        let app2 = new App2<Ran, 'G, 'H>(AppToken<Ran, 'G>.Token token, app)
+        new App3<Ran, 'G, 'H, 'A>(AppToken<App<Ran, 'G>, 'H>.Token app, app2)
+    static member Prj (app3 : App3<Ran, 'G, 'H, 'A>) : Ran<'G, 'H, 'A> =
+        let token' = AppToken<Ran, 'G>.Token token
+        let token'' = AppToken<App<Ran, 'G>, 'H>.Token token'
+        let app2 = app3.Apply(token'') :?> App2<Ran, 'G, 'H>
+        let app = app2.Apply(token') :?> App<Ran, 'G>
+        app.Apply(token) :?> _
+
+type RanFunctor<'G, 'H>() = 
+    inherit Functor<App2<Ran, 'G, 'H>>()
+    
+    override self.Map (f : 'A -> 'B) (c : App3<Ran, 'G, 'H, 'A>) = 
+        Ran.Inj <|
+            { new Ran<'G, 'H, 'B> with
+                member self.Invoke<'R> (k : 'B -> App<'G, 'R>) = 
+                    (Ran.Prj c).Invoke (k << f) }
+        
+
+
+
