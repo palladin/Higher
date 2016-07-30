7957b053b5fb320730186266baa5b580dd34ee33
Nick Palladinos <nick.palladinos@gmail.com>
2016-07-30 15:13:16 +0300

    Add missing Compose Run and refactor Adjunction Monad/Comonad

--- src/Higher.Core/Compose.fs
+++ src/Higher.Core/Compose.fs
@@ -14,13 +14,14 @@ type Compose private () =
         let app2 = app3.Apply(token'') :?> App2<Compose, 'F, 'G>
         let app = app2.Apply(token') :?> App<Compose, 'F>
         app.Apply(token) :?> _
+    static member Run (app3 : App3<Compose, 'F, 'G, 'A>) : App<'F, App<'G, 'A>> = 
+        let (Comp app) = Compose.Prj app3 in app
 
 
 type ComposeFunctor<'F, 'G>(F : Functor<'F>, G : Functor<'G>) = 
     inherit Functor<App2<Compose, 'F, 'G>>()
     override self.Map (f : 'A -> 'B) (app : App3<Compose, 'F, 'G, 'A>) : App3<Compose, 'F, 'G, 'B> = 
-        let (Comp app') = Compose.Prj app
-        Compose.Inj <| Comp (F.Map (G.Map f) app')
+        Compose.Inj <| Comp (F.Map (G.Map f) (Compose.Run app))
         
 
 type ComposeApplicative<'F, 'G>(F : Applicative<'F>, G : Applicative<'G>) = 
@@ -28,9 +29,7 @@ type ComposeApplicative<'F, 'G>(F : Applicative<'F>, G : Applicative<'G>) =
     override self.Pure (v : 'A) : App3<Compose, 'F, 'G, 'A> = 
         Compose.Inj <| Comp  (F.Pure (G.Pure v))
     override self.Apply (f : App<App2<Compose, 'F, 'G>, 'A -> 'B>) (app : App3<Compose, 'F, 'G, 'A>) : App3<Compose, 'F, 'G, 'B> =
-        let (Comp f') = Compose.Prj f
-        let (Comp app') = Compose.Prj app
-        Compose.Inj <| Comp (F.Apply (F.Map G.Apply f') app')
+        Compose.Inj <| Comp (F.Apply (F.Map G.Apply (Compose.Run f)) (Compose.Run app))
         
         
 
